05ea555209e131742e822fdfa38e090ccab3553d
Charity Smith <150025121+csmith709@users.noreply.github.com>
2024-10-08 09:50:52 -0230

    changes

--- Assignment5/MainApp.java
+++ Assignment5/MainApp.java
@@ -2,10 +2,7 @@ package Assignment5;
 
 public class MainApp {
     public static void main(String[] args) {
-        // Create an instance of UndoRedoManager
         UndoRedoManager<String> undoRedoManager = new UndoRedoManager<>();
-
-        // Add states to the manager
         undoRedoManager.addState("State 1");
         undoRedoManager.addState("State 2");
         undoRedoManager.addState("State 3");
@@ -14,15 +11,11 @@ public class MainApp {
         undoRedoManager.addState("State 6");
         undoRedoManager.addState("State 7");
 
-        // Output current state
-        System.out.println("Current State: " + undoRedoManager.currentState.state);
-
-        // Undo a few times
+        // Accessing the current state using the public method
+        System.out.println("Current State: " + undoRedoManager.getCurrentState());
         System.out.println("Undo: " + undoRedoManager.undo());
-        System.out.println("Current State: " + undoRedoManager.currentState.state);
-
-        // Redo once
+        System.out.println("Current State: " + undoRedoManager.getCurrentState());
         System.out.println("Redo: " + undoRedoManager.redo());
-        System.out.println("Current State: " + undoRedoManager.currentState.state);
+        System.out.println("Current State: " + undoRedoManager.getCurrentState());
     }
 }
--- Assignment5/UndoRedoManager.java
+++ Assignment5/UndoRedoManager.java
@@ -5,6 +5,7 @@ public class UndoRedoManager<T> {
         private T state;
         private Node prev;
         private Node next;
+
         private Node(T state) {
             this.state = state;
         }
@@ -56,5 +57,28 @@ public class UndoRedoManager<T> {
             currentState = nextState;
         }
         return currentState.state;
+    }
+
+    // New method to get current state
+    public T getCurrentState() {
+        return currentState != null ? currentState.state : null;
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        UndoRedoManager<String> undoRedoManager = new UndoRedoManager<>();
+        undoRedoManager.addState("State 1");
+        undoRedoManager.addState("State 2");
+        undoRedoManager.addState("State 3");
+        undoRedoManager.addState("State 4");
+        undoRedoManager.addState("State 5");
+        undoRedoManager.addState("State 6");
+        undoRedoManager.addState("State 7");
+
+        System.out.println("Current State: " + undoRedoManager.getCurrentState());
+        System.out.println("Undo: " + undoRedoManager.undo());
+        System.out.println("Current State: " + undoRedoManager.getCurrentState());
+        System.out.println("Redo: " + undoRedoManager.redo());
+        System.out.println("Current State: " + undoRedoManager.getCurrentState());
     }
 }
